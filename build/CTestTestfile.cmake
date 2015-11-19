ac1552b389f2f1b320b8dabb1a84984deb84c9e4
Richard <rdoan@berkeley.edu>
2015-11-19 11:26:06 -0800

    Added all project files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shallowblue/Project/src
+# Build directory: /home/shallowblue/Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(project)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shallowblue/Project/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/shallowblue/Project/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/shallowblue/Project/devel/lib:/home/shallowblue/Project/devel/lib/x86_64-linux-gnu:/home/shallowblue/Project/lab7/devel/lib/x86_64-linux-gnu:/scratch/shared/baxter_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/shallowblue/Project/lab7/devel/lib:/scratch/shared/baxter_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/shallowblue/Project/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/shallowblue/Project/devel/lib/pkgconfig:/home/shallowblue/Project/devel/lib/x86_64-linux-gnu/pkgconfig:/home/shallowblue/Project/lab7/devel/lib/x86_64-linux-gnu/pkgconfig:/scratch/shared/baxter_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/shallowblue/Project/lab7/devel/lib/pkgconfig:/scratch/shared/baxter_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/shallowblue/Project/build"
+export PYTHONPATH="/home/shallowblue/Project/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shallowblue/Project/devel/share/common-lisp:/scratch/shared/baxter_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/shallowblue/Project/src:/scratch/shared/baxter_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
